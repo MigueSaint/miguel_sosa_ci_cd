8fdcd8d6e576601b5960bca231be546ce487d43e
MigueSaint <amg.sosa@yavirac.edu.ec>
2025-11-06 10:23:09 -0500

    Corrección final de tests y app Miguel Sosa

--- app.py
+++ app.py
@@ -7,5 +7,4 @@ def home():
     return "Hola, soy Miguel Sosa y este contenedor se construyó con CI/CD y Docker (v1.0.0)."
 
 if __name__ == "__main__":
-
     app.run(host="0.0.0.0", port=8000)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,24 @@
+import os
+import sys
+import pytest
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from app import app
 
-def test_home_status_code():
-    client = app.test_client()
+
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_status_code(client):
     response = client.get("/")
     assert response.status_code == 200
 
-def test_home_message():
-    client = app.test_client()
+
+def test_home_message(client):
     response = client.get("/")
     assert b"Miguel Sosa" in response.data
